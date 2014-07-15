1834fd424d3776426051aba737f1b2b5c80ccb02
Stephen Fletcher <ste.fletcher@gmail.com>
2014-07-15 22:09:44 +0100

    svg handlebars and js controller updates.

--- js/playwidget.js
+++ js/playwidget.js
@@ -1,21 +1,39 @@
 var widgetDataDefault = {
-    width: 100,
-    radius: 90
+    width: 500
 };
 
-function loadWidgetOn($CONTAINER) {
-    var $container = $CONTAINER;
+function loadWidgetOn($CONTAINER, options) {
+
+    var getCentroid = function(radius, width) {
+        var _width = width / 2;
+        var centroid = {
+            x: _width - (radius / 3),
+            y: _width - ((radius + (radius / 2)) / 3)
+        };
+        return centroid;
+    }
+
+    var defaults = {
+            width: 100,
+            strokeWidth: 10
+        },
+        _this = $CONTAINER,
+        _options = $.extend(defaults, options);
+    _options.radius = _options.width / 2 - _options.strokeWidth / 2;
+    _options.radius -= 5;
+    _options.centroid = getCentroid(_options.radius, _options.width);
+
     $.get('/playwidget.htm',
         function(data, status) {
             console.log("loading template..");
             var renderer = Handlebars.compile(data);
-            console.log(widgetDataDefault);
-            var result = renderer(widgetDataDefault);
-            $container.html(result);
+            console.log(_options);
+            var result = renderer(_options);
+            _this.html(result);
         });
 }
 $(document).ready(function() {
-    loadWidgetOn($('#track1_container'));
+    loadWidgetOn($('#track1_container'), widgetDataDefault);
 });
 
 
